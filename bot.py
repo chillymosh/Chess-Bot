ddf1a9304ba29893c744347c05e3cdf7a72553e8
scottserven <scottserven@gmail.com>
2023-02-25 21:17:54 -0500

    upgraded discord.py from 1.7.3 to 2.0+

--- bot.py
+++ bot.py
@@ -1,22 +1,23 @@
 import os
 import io
+import asyncio
 import random
 import json
 from json import JSONEncoder
 from datetime import datetime
 from dotenv import load_dotenv
 from chess import Board, Move, pgn
-from chess import WHITE, BLACK, SQUARE_NAMES
+from chess import WHITE, BLACK
 from chess import InvalidMoveError, IllegalMoveError, AmbiguousMoveError
 from chess.pgn import StringExporter
 from PIL import Image
 import discord
 from discord.ext import commands
-from discord_slash import cog_ext
-from discord_slash import SlashCommand
-from discord_slash.utils.manage_commands import create_option
-
 from generator import Generator
+
+bot = commands.Bot(command_prefix="",
+                   case_insensitive=True,
+                   intents=discord.Intents.all())
 
 
 class Settings:
@@ -29,17 +30,10 @@ class Settings:
     storage_type = None
     database_url = None
 
-    bot = commands.Bot(command_prefix="!",
-                       case_insensitive=True,
-                       intents=discord.Intents.all())
-    SlashCommand(bot, sync_commands=True)
-
     @classmethod
     def load_env_settings(cls):
         load_dotenv()
 
-        # TODO, there is undoubtedly a better way to handle slash command registration that setting server IDs in the
-        # config and passing that list to the command registrations
         if os.getenv('GUILD_IDS'):
             if ',' in os.getenv('GUILD_IDS'):
                 cls.guild_ids = [int(guild_id) for guild_id in os.getenv('GUILD_IDS').split(',')]
@@ -141,7 +135,7 @@ class Chess(commands.Cog):
         title, description = f"**{title}:**", f"__{description}__"
         embed = discord.Embed(color=0xff0000)
         embed.add_field(name=title, value=description, inline=True)
-        await ctx.send(embed=embed)
+        await ctx.response.send_message(embed=embed)
 
     def _get_member_by_id(self, user_id):
         """
@@ -186,24 +180,9 @@ class Chess(commands.Cog):
                 setattr(new_game, key, game_dict[key])
         return new_game
 
-    @cog_ext.cog_slash(name="new",
-                       description="Start a new match against a specific opponent, or the first to accept",
-                       guild_ids=Settings.guild_ids,
-                       options=[
-                           create_option(
-                               name="opponent",
-                               description="opponent",
-                               option_type=6,  # user
-                               required=False
-                           ),
-                           create_option(
-                               name="rated",
-                               description="Will the game result will be recorded in your stats (default is True)",
-                               option_type=5,  # bool
-                               required=False
-                           )
-                       ])
-    async def new(self, ctx, opponent=None, rated=True):
+    @discord.app_commands.command(name="new",
+                                  description="Start a new match against a specific opponent, or the first to accept")
+    async def new(self, ctx, opponent: discord.Member = None, rated: bool = True):
         """
         Invite a specific user, or make an open invitation to any player to start a match.
         :param ctx:
@@ -211,7 +190,7 @@ class Chess(commands.Cog):
         :param rated:
         :return:
         """
-        if ctx.author == opponent:
+        if ctx.user == opponent:
             return await self.send_error(ctx, description="You cannot challenge yourself")
 
         game_rec = GameStorage.db.get_current_game(ctx.channel_id)
@@ -220,88 +199,76 @@ class Chess(commands.Cog):
 
         GameStorage.db.new_match(ctx.guild_id,
                                  ctx.channel_id,
-                                 ctx.author_id,
+                                 ctx.user.id,
                                  opponent.id if opponent else None,
                                  rated)
 
         rated_msg = "a `RATED`" if rated is True else "an `UNRATED`"
 
         if opponent:
-            message = f"{opponent.mention}, {ctx.author.mention} wants to play a {rated_msg} chess match against you!\n\n"\
+            message = f"{opponent.mention}, {ctx.user.mention} wants to play a {rated_msg} chess match against you!\n\n" \
                       f"Use `/accept` or `/decline`."
         else:
-            message = f"{ctx.author.mention} is looking for any challenger for {rated_msg} chess match! \n\n" \
+            message = f"{ctx.user.mention} is looking for any challenger for {rated_msg} chess match! \n\n" \
                       f"Anyone may use `/accept` to accept the challenge."
 
-        await ctx.send(f"{message}")
+        await ctx.response.send_message(f"{message}")
 
-    @cog_ext.cog_slash(name="decline",
-                       description="decline an active invitation in this channel",
-                       guild_ids=Settings.guild_ids)
+    @discord.app_commands.command(name="decline",
+                                  description="decline an active invitation in this channel")
     async def decline(self, ctx):
         invite = GameStorage.db.get_open_invites(ctx.channel_id)
-        if invite and invite['opponent_id'] == ctx.author_id:
+        if invite and invite['opponent_id'] == ctx.user.id:
             user = self._get_member_by_id(invite['user_id'])
             GameStorage.db.decline_invite(invite['match_id'])
-            await ctx.send(f"You have declined the invite from {user.mention}.")
+            await ctx.response.send_message(f"You have declined the invite from {user.mention}.")
             return
 
         return await self.send_error(ctx, "No invite was found to decline")
 
-    @cog_ext.cog_slash(name="cancel",
-                       description="Cancel a new match that you invited people to in this channel",
-                       guild_ids=Settings.guild_ids)
+    @discord.app_commands.command(name="cancel",
+                                  description="Cancel a new match that you invited people to in this channel")
     async def cancel(self, ctx):
         invite = GameStorage.db.get_open_invites(ctx.channel_id)
-        if invite and invite['user_id'] == ctx.author_id:
+        if invite and invite['user_id'] == ctx.user.id:
             GameStorage.db.cancel_invite(invite['match_id'])
-            await ctx.send(f"You have cancelled your invitation for a new match")
+            await ctx.response.send_message(f"You have cancelled your invitation for a new match")
             return
 
         return await self.send_error(ctx, "You have no open invites exist, so there is nothing to cancel")
 
-    @cog_ext.cog_slash(name="accept",
-                       description="Accept an invite to play chess",
-                       guild_ids=Settings.guild_ids)
+    @discord.app_commands.command(name="accept",
+                                  description="Accept an invite to play chess")
     async def accept(self, ctx):
         invite = GameStorage.db.get_open_invites(ctx.channel_id)
         if invite is None:
             return await self.send_error(ctx, "There are no invites for anybody in this channel.")
+        print("channel_id", ctx.channel_id, "user", ctx.user.id, "invite", invite['user_id'])
 
-        if invite['user_id'] == ctx.author_id:
+        if invite['user_id'] == ctx.user.id:
             return await self.send_error(ctx, "You cannot accept your own invitation")
 
-        if (invite['opponent_id'] is not None) and (invite['opponent_id'] != ctx.author_id):
+        if (invite['opponent_id'] is not None) and (invite['opponent_id'] != ctx.user.id):
             return await self.send_error(ctx, "Sorry, the invite in this channel is for a specific user")
 
         user = self._get_member_by_id(invite['user_id'])
-        white_id, black_id = random.sample([user.id, ctx.author_id], 2)
+        white_id, black_id = random.sample([user.id, ctx.user.id], 2)
 
         game = Game(white_id, black_id, invite['match_id'])
 
         game_state = json.dumps(game, cls=GameEncoder)
-        GameStorage.db.accept_invite(invite['match_id'], ctx.author_id)
+        GameStorage.db.accept_invite(invite['match_id'], ctx.user.id)
         GameStorage.db.save_game_state(invite['match_id'], game_state)
 
         await self.render_game_board(ctx, game)
 
-    @cog_ext.cog_slash(name="move",
-                       description="executes a move during a chess match",
-                       guild_ids=Settings.guild_ids,
-                       options=[
-                           create_option(
-                               name="move",
-                               description="Either SAN (ex. Nf3) or UCI (ex. a2-a3 or a2a3) format",
-                               option_type=3,
-                               required=True
-                           )
-                       ])
-    async def move(self, ctx, move):
+    @discord.app_commands.command(name="move",
+                                  description="executes a move during a chess match")
+    async def move(self, ctx, move: str):
         """
         Handle a move from one of the players.
         :param ctx:
-        :param start:
-        :param end:
+        :param move:
         :return:
         """
         game_rec = GameStorage.db.get_current_game(ctx.channel_id)
@@ -310,11 +277,11 @@ class Chess(commands.Cog):
 
         current_game = await self.convert_game_state_to_game(game_rec['game_state'])
 
-        if ctx.author_id not in [current_game.white_id, current_game.black_id]:
-            await ctx.send("Only the players of the current match can make moves")
+        if ctx.user.id not in [current_game.white_id, current_game.black_id]:
+            await ctx.response.send_message("Only the players of the current match can make moves")
             return
 
-        color = WHITE if self._get_member_by_id(current_game.white_id) == ctx.author else BLACK
+        color = WHITE if self._get_member_by_id(current_game.white_id) == ctx.user else BLACK
         if color is not current_game.board.turn:
             return await self.send_error(ctx, "It is not your turn to make a move")
 
@@ -357,8 +324,8 @@ class Chess(commands.Cog):
                 GameStorage.db.add_user_stats_draw(ctx.guild_id, game_rec['opponent_id'])
 
         if current_game.board.is_checkmate():
-            winner_id = ctx.author_id
-            loser_id = game_rec['opponent_id'] if game_rec['user_id'] == ctx.author_id else game_rec['user_id']
+            winner_id = ctx.user.id
+            loser_id = game_rec['opponent_id'] if game_rec['user_id'] == ctx.user.id else game_rec['user_id']
             GameStorage.db.match_won(game_rec['match_id'], winner_id, loser_id)
             if game_rec['rated'] is True:
                 GameStorage.db.add_user_stats_win(ctx.guild_id, winner_id)
@@ -366,9 +333,8 @@ class Chess(commands.Cog):
 
         await self.render_game_board(ctx, current_game)
 
-    @cog_ext.cog_slash(name="show",
-                       description="Shows the current match chessboard",
-                       guild_ids=Settings.guild_ids)
+    @discord.app_commands.command(name="show",
+                                  description="Shows the current match chessboard")
     async def show(self, ctx):
         """
         Re-shows the current board in cases where the message that contained the board has been deleted.
@@ -381,9 +347,8 @@ class Chess(commands.Cog):
 
         await self.render_game_board(ctx, current_game)
 
-    @cog_ext.cog_slash(name="surrender",
-                       description="Surrender the current match",
-                       guild_ids=Settings.guild_ids)
+    @discord.app_commands.command(name="surrender",
+                                  description="Surrender the current match")
     async def surrender(self, ctx):
         """
         Surrenders the current game, marking is as surrendered and recording a win/loss for the appropriate players.
@@ -395,12 +360,12 @@ class Chess(commands.Cog):
             await self.send_error(ctx, "No active games were found.  Use `/new` to start a new match.")
             return
 
-        if ctx.author_id not in [game_rec['user_id'], game_rec['opponent_id']]:
+        if ctx.user.id not in [game_rec['user_id'], game_rec['opponent_id']]:
             await self.send_error(ctx, "You are not a participant in the current game, so you can't surrender")
             return
 
-        loser_id = ctx.author_id
-        if game_rec['user_id'] == ctx.author_id:
+        loser_id = ctx.user.id
+        if game_rec['user_id'] == ctx.user.id:
             winner_id = game_rec['opponent_id']
         else:
             winner_id = game_rec['user_id']
@@ -413,12 +378,11 @@ class Chess(commands.Cog):
         current_game = await self.convert_game_state_to_game(game_rec['game_state'])
         await self.render_game_board(ctx,
                                      current_game,
-                                     message=f"**{ctx.author.name} has Surrendered!**",
+                                     message=f"**{ctx.user.name} has Surrendered!**",
                                      show_pgn=True)
 
-    @cog_ext.cog_slash(name="leaderboard",
-                       description="Top 10 ChessBot Players",
-                       guild_ids=Settings.guild_ids)
+    @discord.app_commands.command(name="leaderboard",
+                                  description="Top 10 ChessBot Players")
     async def leaderboard(self, ctx):
         leaders = GameStorage.db.get_leaderboard(ctx.guild_id)
 
@@ -426,28 +390,19 @@ class Chess(commands.Cog):
         for idx, leader in enumerate(leaders):
             user = self._get_member_by_id(leader['user_id'])
             if user:
-                embed.add_field(name=f"#{idx+1} - {user.name}",
-                                value=f"> Ratio:  {(leader['win_ratio']*100):.2f}%\n"
+                embed.add_field(name=f"#{idx + 1} - {user.name}",
+                                value=f"> Ratio:  {(leader['win_ratio'] * 100):.2f}%\n"
                                       f"> Wins:   {leader['wins']}\n"
                                       f"> Losses: {leader['losses']}",
                                 inline=False)
 
-        return await ctx.send(embed=embed)
+        return await ctx.response.send_message(embed=embed)
 
-    @cog_ext.cog_slash(name="stats",
-                       description="Shows the statistics for yourself or another user",
-                       guild_ids=Settings.guild_ids,
-                       options=[
-                           create_option(
-                               name="user",
-                               description="Other user",
-                               option_type=6,  # user
-                               required=False
-                           )
-                       ])
-    async def statistics(self, ctx, user=None):
+    @discord.app_commands.command(name="stats",
+                                  description="Shows the statistics for yourself or another user")
+    async def statistics(self, ctx, user: discord.User = None):
         if user is None:
-            user = ctx.author
+            user = ctx.user
 
         stats = GameStorage.db.get_user_stats(ctx.guild_id, user.id)
         if stats is None:
@@ -458,10 +413,10 @@ class Chess(commands.Cog):
             }
 
         embed = discord.Embed(color=0x0000ff, title=f"Chess Stats for {str(user)}")
-        embed.add_field(name="Ratio ", value=f"{stats['win_ratio']*100:.2f}%", inline=False)
+        embed.add_field(name="Ratio ", value=f"{stats['win_ratio'] * 100:.2f}%", inline=False)
         embed.add_field(name="Wins  ", value=f"{stats['wins']}", inline=False)
         embed.add_field(name="Losses", value=f"{stats['losses']}", inline=False)
-        await ctx.send(embed=embed)
+        await ctx.response.send_message(embed=embed)
 
     async def render_game_board(self, ctx, current_game, message=None, show_pgn=False):
         """
@@ -469,6 +424,8 @@ class Chess(commands.Cog):
         and the most recent move.
         :param ctx:
         :param current_game:
+        :param message:
+        :param show_pgn:
         :return:
         """
         white_user = self._get_member_by_id(current_game.white_id)
@@ -518,7 +475,7 @@ class Chess(commands.Cog):
         board_image = Chess.get_binary_board(current_game.board)
         embed.set_image(url="attachment://board.jpg")
 
-        await ctx.send(embed=embed, file=board_image)
+        await ctx.response.send_message(embed=embed, file=board_image)
 
     @staticmethod
     def get_binary_board(board) -> discord.File:
@@ -547,24 +504,28 @@ class Chess(commands.Cog):
         return export.accept(StringExporter(headers=False, columns=None))
 
 
-@Settings.bot.event
+@bot.event
 async def on_ready() -> None:
     """
     Occurs when the Bot connects to Discord.  Outputs startup messages to the console.
     :return:
     """
     print("The BOT is currently online, connect to a Discord Server which contains it to start playing!")
-    print("The name of the BOT is:", Settings.bot.user.name)
-    print("The ID of the BOT is:", Settings.bot.user.id)
+    print("The name of the BOT is:", bot.user.name)
+    print("The ID of the BOT is:", bot.user.id)
     print("Running for the following servers: ")
-    for guild in Settings.guild_ids:
-        print(" -", Settings.bot.get_guild(guild))
+    for guild_id in Settings.guild_ids:
+        guild = bot.get_guild(guild_id)
+        print(" -", guild)
+        await bot.tree.sync(guild=None)
+        print("Commands synced")
 
-    print(datetime.now().strftime("%d-%m-%Y %H:%M"))
 
-
-if __name__ == '__main__':
+async def main():
     Settings.init()
     GameStorage.init()
-    Settings.bot.add_cog(Chess(Settings.bot))
-    Settings.bot.run(Settings.bot_token)
+    await bot.add_cog(Chess(bot))
+
+
+asyncio.run(main())
+bot.run(Settings.bot_token)
